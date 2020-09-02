c80422d7380e32538aaeccdd044bd58d8613a1e3
zrc1708 <1023054043@qq.com>
2020-09-02 23:29:42 +0800

    项目整理，发布到npm

--- examples/main.js
+++ examples/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import niUI from './components/index'
+import niUI from '../packages/index'
 
 Vue.config.productionTip = false
 
